dcf6272f5951c9da5abe66d3ae1b0004c230ef81
Vinco Wu <vwu13@outlook.com>
2021-09-22 12:17:32 -0400

    .env sample file updated

--- market-stats-vinco-wu/client/src/components/SignUpModal/SignUpModal.js
+++ market-stats-vinco-wu/client/src/components/SignUpModal/SignUpModal.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from '../../assets/logo/Market-Stats-logo.png';
 import close from '../../assets/icons/close_black_24dp.svg'
 import { Modal } from '@material-ui/core';
+import { Redirect, withRouter } from 'react-router-dom';
 import axios from 'axios';
 import { API_URL } from '../../util';
 
@@ -19,6 +20,7 @@ export class SignUpModal extends Component {
             email: this.state.formInfo.email,
             password: this.state.formInfo.password
         })
+            .then(this.props.redirect);
     }
 
     render() {
@@ -32,14 +34,14 @@ export class SignUpModal extends Component {
                         <img className="close" src={close} onClick={this.props.closing} />
                     </div>
                     <h2 className="welcome-message">Welcome New Investors!</h2>
-                    <form onSubmit={this.formSubmit} className="login-form">
+                    <form className="login-form">
                         {/* Email */}
                         <input type="email" name="email" onChange={this.formChange} placeholder="Your Email" className="login-form__text-box" />
                         {/* Password */}
                         <input type="password" name="password" onChange={this.formChange} placeholder="Your Password" className="login-form__text-box" />
                         {/* Button Container */}
                         <div className="buttons-container">
-                            <button className="form-buttons form-buttons--orange" type="button" onClick={this.signup}>
+                            <button className="form-buttons form-buttons--orange" type="button" onClick={this.formSubmit}>
                                 Create Account
                             </button>
                         </div>
@@ -50,4 +52,4 @@ export class SignUpModal extends Component {
     }
 }
 
-export default SignUpModal
+export default withRouter(SignUpModal)
